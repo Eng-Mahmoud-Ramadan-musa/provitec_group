7890484f061587d6629cd0d5dc8133eb57f91b1a
Eng-Mahmoud-Ramadan-musa <160401248+Eng-Mahmoud-Ramadan-musa@users.noreply.github.com>
2025-08-16 17:09:19 +0300

    delete unused ui components

--- src/app/components/ui/HeaderSection.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function HeaderSection({title}: {title: string}) {
-
-  return (
-    <div className="px-4 rounded-lg bg-green-500 text-white font-bold">
-      <h2 className="text-2xl">{title}</h2>
-    </div>
-  )
-}
